6bd65152c8a2a6f947ed2ca00daa0d3b89637d21
Desenvolvimento <Desenvolvimento@DESKTOP-1AKFVFH>
2021-09-28 17:28:29 -0300

    teste de commit

---
+++ Financeiro/src/main/java/br/com/vipautomacao/domain/model/Banco.java
@@ -0,0 +1,25 @@
+package br.com.vipautomacao.domain.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "banco")
+public class Banco {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="CODIGO"  )
+	private Integer codigo;
+	@Column(name="nome"  )
+	private String nome;
+}
