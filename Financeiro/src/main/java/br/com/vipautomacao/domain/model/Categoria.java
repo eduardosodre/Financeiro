2f71e3a3f2213d87127c9c5deadbe1cb2f8d766e
GabrielTasquin <tasquingabriel1@gmail.com>
2021-09-29 22:39:56 -0300

    teste commit

--- Financeiro/src/main/java/br/com/vipautomacao/domain/model/Categoria.java
+++ Financeiro/src/main/java/br/com/vipautomacao/domain/model/Categoria.java
@@ -1,5 +1,15 @@
 package br.com.vipautomacao.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "categoria")
 public class Categoria {
 
 }
